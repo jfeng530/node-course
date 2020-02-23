50f4bb3f821b176d7e95ac19926aeec9d755acc9
github username <github email address>
2020-02-22 21:20:47 -0500

    importing own files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
-// import/load 'fs' module and saves return value to const 'fs'
+// --- Importing Modules ---
 
-// fs.writeFileSync('notes.txt', 'My name is Jacky.')
-// first arg: string => filename, second arg: string => data
+// const fs = require('fs')
+// // import/load 'fs' module and saves return value to const 'fs'
 
-fs.appendFileSync('notes.txt', ' Testing append-file-sync')
+// // fs.writeFileSync('notes.txt', 'My name is Jacky.')
+// // first arg: string => filename, second arg: string => data
+
+// fs.appendFileSync('notes.txt', ' Testing append-file-sync')
+
+// --- Importing Own Files ---
+
+// const addFunc = require('./utils.js')
+// // import from utils.js, save return values to const
+
+// const sum = addFunc(4, -2)
+
+// console.log(sum)
+
+const getNotes = require('./notes')
+
+const notes = getNotes()
+
+console.log(notes)
---
+++ notes-app/utils.js
@@ -0,0 +1,10 @@
+// console.log('utils.js')
+
+// const name = 'Feng'
+
+// const add = function(a, b) {
+//   return a + b
+// }
+
+// module.exports = add
+// // export values
