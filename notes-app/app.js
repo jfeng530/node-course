f9a2b3d50ab67b0e1fd918b092c0ec6eae72b6bd
github username <github email address>
2020-02-22 21:05:10 -0500

    appendFileSync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 // import/load 'fs' module and saves return value to const 'fs'
 
-fs.writeFileSync('notes.txt', 'My name is Jacky.')
+// fs.writeFileSync('notes.txt', 'My name is Jacky.')
 // first arg: string => filename, second arg: string => data
+
+fs.appendFileSync('notes.txt', ' Testing append-file-sync')
