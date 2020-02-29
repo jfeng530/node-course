a588a5e76158ae3362826634f7924ebefb9d7baa
github username <github email address>
2020-02-29 17:30:20 -0500

    json manipulation

--- playground/1-json.js
+++ playground/1-json.js
@@ -7,10 +7,20 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(book) // turns JS object into 'string'
 // fs.writeFileSync('1-json.json', bookJSON)
 
-const dataBuffer = fs.readFileSync('1-json.json') // buffer returned
-const dataJSON = dataBuffer.toString() // converts buffer to string
-const data = JSON.parse(dataJSON)
-console.log(data.title)
+// const dataBuffer = fs.readFileSync('1-json.json') // buffer returned
+// const dataJSON = dataBuffer.toString() // converts buffer to string
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Jacky'
+user.age = 25
+
+const newUser = JSON.stringify(user)
+fs.writeFileSync('1-json.json', newUser)
 
 // console.log(bookJSON)
 
